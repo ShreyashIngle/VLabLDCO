0e0f128d1e7ae769fa7b8666cb559a666d4e9954
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2024-02-25 00:41:00 +0530

    major updates

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Get all the experiment list items
-  const experimentItems = document.querySelectorAll(".experiment-description ");
+  const experimentItems = document.querySelectorAll(".experiment");
 
   // Add a click event listener to each experiment list item
-  experimentItems.forEach((item) => {
+  experimentItems.forEach((item, index) => {
     item.addEventListener("click", () => {
-      // Redirect to the experiment page
-      window.location.href = "experiments.html";
+      // Redirect to the experiment page with the experiment ID in the URL
+      window.location.href = `./experiments/experiment${index + 1}.html`;
     });
   });
-};
-
-window.onload = function () {
-  // Get all the experiment list items
-  const experimentItems = document.querySelectorAll(".experiment-title");
-
-  // Add a click event listener to each experiment list item
-  experimentItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      // Get the experiment title and ID
-      const experimentTitle = item.innerText;
-      const experimentId = item.dataset.experimentId;
-
-      // Redirect to the experiment page with the experiment title and ID in the URL
-      window.location.href = `experiments.html?title=${encodeURIComponent(
-        experimentTitle
-      )}&id=${encodeURIComponent(experimentId)}`;
-    });
-  });
-};
+});
